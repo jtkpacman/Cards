1273888ffafea553fc51e58402df4dc2f06253dd
Jacob <jtkenyon051992@gmail.com>
2020-03-30 18:46:51 -0400

    First Commit

---
+++ Cards.py
@@ -0,0 +1,48 @@
+# Created by Jacob Kenyon. 3/30/2020
+
+from random import shuffle
+
+
+# The Card class defines what a card is.
+# __init__ methods are similar to constructors in java.
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+
+# Lists containing suits and ranks.
+suit = ['H', 'D', 'S', 'C']
+rank = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
+
+
+# creates the deck and returns a list of cards.
+def createDeck():
+    deck = []
+
+    for i in range(len(suit)):
+        for x in range(len(rank)):
+            deck.append(Card(suit[i], rank[x]))
+
+    print("Created a deck.")
+    return deck
+
+
+# Shuffle the deck
+def shuffleDeck(deck):
+    shuffle(deck)
+    print("Shuffled the deck.")
+
+
+# Prints the current deck.
+def showDeck(deck):
+    for i in range(len(deck)):
+        print("card", i + 1)
+        print("The rank is: ", deck[i].rank, "\nThe suit is: ", deck[i].suit)
+
+
+# Start the game operations.
+userDeck = createDeck()
+showDeck(userDeck)
+shuffleDeck(userDeck)
+showDeck(userDeck)
